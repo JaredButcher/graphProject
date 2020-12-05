3d3be13a139c67a57bf789f7b56bca6a033d75d6
JaredButcher <jared.butcher1219@gmail.com>
2020-12-04 22:50:36 -0500

    Working, processing time from 2 hours to 5 minutes

--- sideEffects/extensions/accel.cpp
+++ sideEffects/extensions/accel.cpp
@@ -2,6 +2,11 @@
 #include <algorithm>
 #include <iostream>
 #include <cstring>
+#include <vector>
+#include <unordered_map>
+#include <list>
+#include <set>
+#include <iterator>
 
 //About 10% faster
 static PyObject *edgeComp(PyObject *self, PyObject *args){
@@ -30,8 +35,8 @@ static PyObject *weight(PyObject *self, PyObject *args){
     PyObject* node1Edges = PyList_GetItem(nodeEdgeVector, node1);
     PyObject* node2Edges = PyList_GetItem(nodeEdgeVector, node2);
 
-    int node1Len = PyList_Size(node1Edges);
-    int node2Len = PyList_Size(node2Edges);
+    int node1Len = static_cast<int>(PyList_Size(node1Edges));
+    int node2Len = static_cast<int>(PyList_Size(node2Edges));
 
     int weight = 0;
 
@@ -66,12 +71,145 @@ static PyObject *weight(PyObject *self, PyObject *args){
     return Py_BuildValue("d", (double)weight / total);
 }
 
+//(nodeCount, edgeCount, edges)
+static PyObject *buildNodeEdgeVector(PyObject *self, PyObject *args){
+    int nodeCount, edgeCount;
+    Py_buffer edgeBuf;
+    if (!PyArg_ParseTuple(args, "iiy*", &nodeCount, &edgeCount, &edgeBuf)) {
+        PyErr_SetString(PyExc_RuntimeError, "buildNodeEdgeVector incorrect arguments");
+        return NULL;
+    }
+
+    //Initalize nodeEdgeVector
+    auto nodeEdgeVector = new std::vector<std::unordered_map<int, std::list<int>>>();
+    nodeEdgeVector->reserve(nodeCount);
+    for(int i = 0; i < nodeCount; ++i){
+        nodeEdgeVector->push_back(std::unordered_map<int, std::list<int>>());
+    }
+
+    //For each edge, add the connected node to the other node's list for both nodes
+    int* edges = static_cast<int*>(edgeBuf.buf);
+    for(int i = 0; i < edgeCount; ++i){
+        (*nodeEdgeVector)[edges[i * 3]].try_emplace(edges[i * 3 + 2], std::list<int>());
+        (*nodeEdgeVector)[edges[i * 3]][edges[i * 3 + 2]].push_back(edges[i * 3 + 1]);
+
+        (*nodeEdgeVector)[edges[i * 3 + 1]].try_emplace(edges[i * 3 + 2], std::list<int>());
+        (*nodeEdgeVector)[edges[i * 3 + 1]][edges[i * 3 + 2]].push_back(edges[i * 3]);
+    }
+
+    PyBuffer_Release(&edgeBuf);
+
+    PyObject* nodeEdgeVectorCapsule = PyCapsule_New((void*)nodeEdgeVector, "nodeEdgeVectorPtr", NULL);
+    PyCapsule_SetPointer(nodeEdgeVectorCapsule, (void*)nodeEdgeVector);
+
+    return Py_BuildValue("O", nodeEdgeVectorCapsule);
+}
+
+static int nodeEdgeCount(std::unordered_map<int, std::list<int>> &nodeEdges){
+    int count = 0;
+    for(auto i = nodeEdges.begin(); i != nodeEdges.end(); ++i){
+        count += static_cast<int>(i->second.size());
+    }
+    return count;
+}
+
+static double weight(int node1, int node2, std::vector<std::unordered_map<int, std::list<int>>> &nodeEdgeVector){
+    auto node1UniqueKeys = std::set<int>();
+    auto node2UniqueKeys = std::set<int>();
+    std::transform(nodeEdgeVector[node1].begin(), nodeEdgeVector[node1].end(), std::inserter(node1UniqueKeys, node1UniqueKeys.begin()), [](auto pair){return pair.first;});
+    std::transform(nodeEdgeVector[node2].begin(), nodeEdgeVector[node2].end(), std::inserter(node2UniqueKeys, node2UniqueKeys.begin()), [](auto pair){return pair.first;});
+
+    auto commonSymptoms = std::vector<int>();
+    std::set_intersection(node1UniqueKeys.begin(), node1UniqueKeys.end(), node2UniqueKeys.begin(), node2UniqueKeys.end(), std::inserter(commonSymptoms, commonSymptoms.end()));
+        
+    int equalEdges = 0;
+    for(auto i = commonSymptoms.begin(); i != commonSymptoms.end(); ++i){
+        for(auto edge1 = nodeEdgeVector[node1][*i].begin(); edge1 != nodeEdgeVector[node1][*i].end(); ++edge1){
+            for(auto edge2 = nodeEdgeVector[node2][*i].begin(); edge2 != nodeEdgeVector[node2][*i].end(); ++edge2){
+                if(node1 == *edge2 || node2 == *edge1){
+                   equalEdges += 2;
+                   break; 
+                }else if(*edge1 == *edge2){
+                    equalEdges += 1;
+                   break; 
+                }
+            }
+        }
+    }
+
+    int total = (nodeEdgeCount(nodeEdgeVector[node1]) + nodeEdgeCount(nodeEdgeVector[node2]));
+    return (double)equalEdges / (total > 0 ? total : 1);
+}
+
+struct WeightEdge{
+    int n1;
+    int n2;
+    double weight;
+    WeightEdge(int n1, int n2, double weight): n1(n1), n2(n2), weight(weight) {}
+};
+
+static PyObject* buildWeightGraph(PyObject *self, PyObject *args){
+    int threadNum = 0, threads = 1;
+    PyObject* nodeEdgeVectorObj;
+    if (!PyArg_ParseTuple(args, "O|ii", &nodeEdgeVectorObj, &threadNum, &threads)) {
+        PyErr_SetString(PyExc_RuntimeError, "buildWeightGraph incorrect arguments");
+        return NULL;
+    }
+
+    auto nodeEdgeVector = static_cast<std::vector<std::unordered_map<int, std::list<int>>>*>(PyCapsule_GetPointer(nodeEdgeVectorObj, "nodeEdgeVectorPtr"));
+    int nodeCount = static_cast<int>(nodeEdgeVector->size());
+    //nodeCount = 30; //reduce run time for debug
+    auto weightGraph = new std::list<WeightEdge>();
+    PyObject* weightGraphCapsule = PyCapsule_New((void*)weightGraph, "weightGraphPtr", NULL);
+    PyCapsule_SetPointer(weightGraphCapsule, (void*)weightGraph);
+
+    Py_BEGIN_ALLOW_THREADS //Release global interpreter lock
+    for(int i = threadNum; i < nodeCount; i += threads){
+        for(int j = i + 1; j < nodeCount; ++j){
+            weightGraph->push_back(WeightEdge(i, j, weight(i, j, *nodeEdgeVector)));
+        }
+        if(threadNum == 0){
+            std::cout << "\r" << ((double)i / nodeCount) * 100 << "% complete" << std::flush;
+        }
+    }
+    if(threadNum == 0){
+        std::cout << std::endl;
+    }
+
+    Py_END_ALLOW_THREADS //Reaquire global interpreter lock
+    return weightGraphCapsule;
+}
+
+static PyObject* mergeWeightGraphs(PyObject *self, PyObject *args){
+    PyObject* graphs;
+    if (!PyArg_ParseTuple(args, "O", &graphs)) {
+        PyErr_SetString(PyExc_RuntimeError, "mergeWeightGraphs incorrect arguments");
+        return NULL;
+    }
+
+    PyObject* sumWeightGraph = PyList_New(0);
+    for(int i = 0; i < PyList_GET_SIZE(graphs); ++i){
+        auto subgraph = static_cast<std::list<WeightEdge>*>(PyCapsule_GetPointer(PyList_GET_ITEM(graphs, i), "weightGraphPtr"));
+        for(auto j = subgraph->begin(); j != subgraph->end(); ++j){
+            PyList_Append(sumWeightGraph, Py_BuildValue("(iid)", j->n1, j->n2, j->weight));
+        }
+    }
+
+    return sumWeightGraph;
+}
+
 //Module defs
 static PyMethodDef accel_methods[] = {
     {"weight",  weight, METH_VARARGS,
      "Compute weight of simularity between nodes"},
     {"edgeComp",  edgeComp, METH_VARARGS,
      "Compare edges"},
+    {"buildNodeEdgeVector",  buildNodeEdgeVector, METH_VARARGS,
+     "Build a vector of dictionaries of edges keyed by symptom \n(nodeCount, edgeCount, edges)"},
+    {"buildWeightGraph",  buildWeightGraph, METH_VARARGS,
+     "Build the weight graph, supports theading \n(nodeEdgeVector, threadNum=0, threads=1)"},
+    {"mergeWeightGraphs",  mergeWeightGraphs, METH_VARARGS,
+     "Merge list of weight subgraphs \n(graphs)"},
     {NULL, NULL, 0, NULL}        /* Sentinel */
 };
 
--- sideEffects/processing.py
+++ sideEffects/processing.py
@@ -3,118 +3,119 @@ import sys
 import os
 import sideEffects.accel as accel
 import time
-import multiprocessing
+import concurrent.futures
+import threading
+import ctypes
+import itertools
 
 def loadBaseGraph(rebuild=False):
     filename = './ChChSe-Decagon_polypharmacy.csv'
-    nodefilename = './nodes.csv'
+    nodeListfilename = './nodelist.csv'
     edgefilename = './edges.csv'
+    edgeListfilename = './edgelist.csv'
 
-    nodes = []
+    nodeList = []
+    edgeList = []
     edges = []
 
     nodeCount = 0
     edgeCount = 0
+    edgeValueCount = 0
 
-    if rebuild or not os.path.exists(nodefilename) or not os.path.exists(edgefilename):
+    if rebuild or not os.path.exists(nodeListfilename) or not os.path.exists(edgeListfilename) or not os.path.exists(edgefilename):
         print("Building lists")
+        nodeDict = {}
+        edgeDict = {}
         for entry in open(filename, 'r'):
             if entry[0] != '#': #column names
                 values = entry.split(',')
-                if not values[0] in nodes:
-                    nodes.append(values[0])
+                if not values[0] in nodeDict:
+                    nodeList.append(values[0])
+                    nodeDict[values[0]] = nodeCount
                     nodeCount += 1
-                if not values[1] in nodes:
-                    nodes.append(values[1])
+                if not values[1] in nodeDict:
+                    nodeList.append(values[1])
+                    nodeDict[values[1]] = nodeCount
                     nodeCount += 1
-                edges.append((nodes[values[0]], nodes[values[1]], values[2], values[3][:-1]))
+                if not values[2] in edgeDict:
+                    edgeList.append((values[2], values[3][:-1]))
+                    edgeDict[values[2]] = edgeValueCount
+                    edgeValueCount += 1
+                edges.append((nodeDict[values[0]], nodeDict[values[1]], edgeDict[values[2]]))
                 edgeCount += 1
             if not edgeCount % 100000:
                 sys.stdout.write(f'\r{edgeCount} edges')
 
-        sys.stdout.write(f'\r{edgeCount} edges')
-        print("\nSaving nodes")
-
-        with open(nodefilename, 'w') as nodefile:
-            for entry in nodes:
+        print("\nSaving node values")
+        with open(nodeListfilename, 'w') as nodefile:
+            for entry in nodeList:
                 nodefile.write(f'{str(entry)}\n')
 
-        print("Saving edges")
+        print("Saving edge values")
+        with open(edgeListfilename, 'w') as edgeValueFile:
+            for entry in edgeList:
+                edgeValueFile.write(f'{str(entry[0])},{entry[1]}\n')
 
+        print("Saving edges")
         with open(edgefilename, 'w') as edgefile:
             for entry in edges:
-                edgefile.write(f'{str(entry[0])},{str(entry[1])},{str(entry[2])},{str(entry[3])}\n')
+                edgefile.write(f'{str(entry[0])},{str(entry[1])},{str(entry[2])}\n')
     else:
         print("Loading edges")
         for entry in open(edgefilename, 'r'):
             values = entry.split(',')
-            edges.append((int(values[0]), int(values[1]), values[2], values[3]))
+            edges.append((int(values[0]), int(values[1]), int(values[2])))
             edgeCount += 1
             if not edgeCount % 100000:
                 sys.stdout.write(f'\r{edgeCount} edges')
         
-        print("\nLoading nodes")
-        for entry in open(nodefilename, 'r'):
-            nodes.append(entry)
-    
-    return (edges, nodes)
+        print("\nLoading node values")
+        for entry in open(nodeListfilename, 'r'):
+            nodeList.append(entry)
 
-def buildNodeEdgeVector(nodeCount, edges):
-    nodeEdgeVector = [{} for i in range(0, nodeCount)]
-    for edge in edges:
-        if edge[2] in nodeEdgeVector[edge[0]]:
-            nodeEdgeVector[edge[0]][edge[2]].append(edge)
-        else:
-            nodeEdgeVector[edge[0]][edge[2]] = [edge]
-        if edge[2] in nodeEdgeVector[edge[1]]:
-            nodeEdgeVector[edge[1]][edge[2]].append(edge)
-        else:
-            nodeEdgeVector[edge[1]][edge[2]] = [edge]
-    return nodeEdgeVector
+        print("Loading edge values")
+        for entry in open(edgeListfilename, 'r'):
+            edgeList.append(entry)
 
-def edgeComp(edge1, edge2):
-    return edge1[0] == edge2[0] or edge1[0] == edge2[1] or edge1[1] == edge2[0] or edge1[1] == edge2[1]
+    arrayEdges = (ctypes.c_int32 * (3 * edgeCount))(*itertools.chain(*edges))
+    print("Edge array constructred")
 
-def weight(node1, node2, nodeEdgeVector):
-    equalEdges = 0
-
-    sympts = set(nodeEdgeVector[node1].keys()).intersection(nodeEdgeVector[node2].keys())
-
-    for sympt in sympts:
-        for edge1 in nodeEdgeVector[node1][sympt]:
-            for edge2 in nodeEdgeVector[node2][sympt]:
-                if edgeComp(edge1, edge2):
-                    equalEdges += 1
-                    break
-    return equalEdges / (len(nodeEdgeVector[node1]) + len(nodeEdgeVector[node2]))
+    return (arrayEdges, edgeCount, nodeList, edgeList)
 
 #Will proably take about 2 hours to run
-def genWeightedGraph(processes = 1):
+def genWeightedGraph(threads = 10):
     weightedGraphFilename = "weighted.csv"
 
-    edges, nodes = loadBaseGraph()
+    edges, edgeCount, nodeList, edgeList = loadBaseGraph()
     #baseGraph = snap.LoadEdgeList_PUndirNet('edges.csv', 0, 1, ',')
-    nodeCount = len(nodes)
+    nodeCount = len(nodeList)
 
-    print("Building node edge vector")
-    nodeEdgeVector = buildNodeEdgeVector(nodeCount, edges)
+    print("Constructing nodeEdgeVector")
+    nodeEdgeVector = accel.buildNodeEdgeVector(nodeCount, edgeCount, edges)
+    print("NodeEdgeVector constructed, building weighted graph")
 
-    if processes > 2:
-        print("Multiprocessing still a work in progress")
-    else:
-        print("Creating weight graph")
-        weightedEdges = []
-        currentNode = 0
-        for i in range(0,nodeCount):
-            for j in range(i + 1,nodeCount):
-                weightedEdges.append((i, j, weight(i, j, nodeEdgeVector)))
-                sys.stdout.write(f'\r{i * nodeCount + j}/{nodeCount**2} nodes weighted')
-        print("\n Weight graph created")
+    subGraphs = []
+    subGraphLock = threading.Lock()
 
-        with open(weightedGraphFilename, 'w') as weightedGraphFile:
-            for edge in weightedEdges:
-                weightedGraphFile.write(f'{str(weightedEdges[0])},{str(weightedEdges[1])},{str(weightedEdges[2])}\n')
-        print("File saved")
+    def runBuildWeightGraph(threadNumber):
+        print(f'Starting thread {threadNumber}')
+        weightedGraph = accel.buildWeightGraph(nodeEdgeVector, threadNumber, threads)
+        with subGraphLock:
+            print(f'Thread {threadNumber} complete')
+            subGraphs.append(weightedGraph)
+
+    with concurrent.futures.ThreadPoolExecutor(threads) as executor:
+        print(f'Starting {threads} threads')
+        for result in executor.map(runBuildWeightGraph, range(0,threads)):
+            pass
+
+    weightedGraph = accel.mergeWeightGraphs(subGraphs)
+
+    print("Weighted Graph built, writeing to file")
+    with open("weight.csv", "w") as weightFile:
+        for entry in weightedGraph:
+            weightFile.write(f'{str(entry[0])},{str(entry[1])},{str(entry[2])}\n')
+    print("Complete")
 
 def main():
     genWeightedGraph()
