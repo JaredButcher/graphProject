db8e82cd32b07ae196a0cd0739189ba66a8881e7
Jared Butcher <jared.butcher1219@gmail.com>
2020-12-06 15:41:53 -0500

    weight graph clustering

--- sideEffects/processing.py
+++ sideEffects/processing.py
@@ -8,13 +8,9 @@ import threading
 import ctypes
 import itertools
 
-def loadBaseGraph(rebuild=False):
+def loadBaseGraph(snapFilename='./ChChSe-Decagon_polypharmacy.csv', nodeListfilename = './nodelist.csv', edgefilename = './edges.csv', edgeListfilename = './edgelist.csv', rebuild=False):
     '''Loads in the needed files, generatres more useable formats if they don't exist
     '''
-    filename = './ChChSe-Decagon_polypharmacy.csv'
-    nodeListfilename = './nodelist.csv'
-    edgefilename = './edges.csv'
-    edgeListfilename = './edgelist.csv'
 
     nodeList = []
     edgeList = []
@@ -28,25 +24,26 @@ def loadBaseGraph(rebuild=False):
         print("Building lists")
         nodeDict = {}
         edgeDict = {}
-        for entry in open(filename, 'r'):
-            if entry[0] != '#': #column names
-                values = entry.split(',')
-                if not values[0] in nodeDict:
-                    nodeList.append(values[0])
-                    nodeDict[values[0]] = nodeCount
-                    nodeCount += 1
-                if not values[1] in nodeDict:
-                    nodeList.append(values[1])
-                    nodeDict[values[1]] = nodeCount
-                    nodeCount += 1
-                if not values[2] in edgeDict:
-                    edgeList.append((values[2], values[3][:-1]))
-                    edgeDict[values[2]] = edgeValueCount
-                    edgeValueCount += 1
-                edges.append((nodeDict[values[0]], nodeDict[values[1]], edgeDict[values[2]]))
-                edgeCount += 1
-            if not edgeCount % 100000:
-                sys.stdout.write(f'\r{edgeCount} edges')
+        with open(snapFilename, 'r') as snapFile:
+            for entry in snapFile:
+                if entry[0] != '#': #column names
+                    values = entry.split(',')
+                    if not values[0] in nodeDict:
+                        nodeList.append(values[0])
+                        nodeDict[values[0]] = nodeCount
+                        nodeCount += 1
+                    if not values[1] in nodeDict:
+                        nodeList.append(values[1])
+                        nodeDict[values[1]] = nodeCount
+                        nodeCount += 1
+                    if not values[2] in edgeDict:
+                        edgeList.append((values[2], values[3][:-1]))
+                        edgeDict[values[2]] = edgeValueCount
+                        edgeValueCount += 1
+                    edges.append((nodeDict[values[0]], nodeDict[values[1]], edgeDict[values[2]]))
+                    edgeCount += 1
+                if not edgeCount % 100000:
+                    sys.stdout.write(f'\r{edgeCount} edges')
 
         print("\nSaving node values")
         with open(nodeListfilename, 'w') as nodefile:
@@ -82,52 +79,58 @@ def loadBaseGraph(rebuild=False):
     return (edges, nodeList, edgeList)
 
 #Will proably take about  hours to run
-def genWeightedGraph(edges, nodeList, threads = 1):
+def genWeightedGraph(edges, nodeList, threads = 1, weightedGraphFilename = "weighted.csv", rebuild=False):
     '''Generates a complete graph of the drug nodes and the edges storing weights from 0-1 
         representing how similar their common symptom interactions are.
         Will take about 30 min / threads to run with on a desktop processor
     '''
-    weightedGraphFilename = "weighted.csv"
+    weightedGraph = []
 
-    nodeCount = len(nodeList)
-    edgeCount = len(edges)
+    if rebuild or not os.path.exists(weightedGraphFilename):
+        nodeCount = len(nodeList)
+        edgeCount = len(edges)
 
-    print("Converting edges to c array")
-    arrayEdges = (ctypes.c_int32 * (3 * edgeCount))(*itertools.chain(*edges))
-    print("Edge array constructred")
+        print("Converting edges to c array")
+        arrayEdges = (ctypes.c_int32 * (3 * edgeCount))(*itertools.chain(*edges))
+        print("Edge array constructred")
 
-    print("Constructing nodeEdgeVector")
-    nodeEdgeVector = accel.buildNodeEdgeVector(nodeCount, edgeCount, arrayEdges)
-    print("NodeEdgeVector constructed, building weighted graph")
+        print("Constructing nodeEdgeVector")
+        nodeEdgeVector = accel.buildNodeEdgeVector(nodeCount, edgeCount, arrayEdges)
+        print("NodeEdgeVector constructed, building weighted graph")
 
-    subGraphs = []
-    subGraphLock = threading.Lock()
+        subGraphs = []
+        subGraphLock = threading.Lock()
 
-    def runBuildWeightGraph(threadNumber):
-        print(f'Starting thread {threadNumber}')
-        weightedGraph = accel.buildWeightGraph(nodeEdgeVector, threadNumber, threads)
-        with subGraphLock:
-            print(f'Thread {threadNumber} complete')
-            subGraphs.append(weightedGraph)
+        def runBuildWeightGraph(threadNumber):
+            print(f'Starting thread {threadNumber}')
+            weightedGraph = accel.buildWeightGraph(nodeEdgeVector, threadNumber, threads)
+            with subGraphLock:
+                print(f'Thread {threadNumber} complete')
+                subGraphs.append(weightedGraph)
 
-    with concurrent.futures.ThreadPoolExecutor(threads) as executor:
-        print(f'Starting {threads} threads')
-        for result in executor.map(runBuildWeightGraph, range(0,threads)):
-            pass
+        with concurrent.futures.ThreadPoolExecutor(threads) as executor:
+            print(f'Starting {threads} threads')
+            for result in executor.map(runBuildWeightGraph, range(0,threads)):
+                pass
 
-    print("Converting into python friendly format")
-    weightedGraph = accel.mergeWeightGraphs(subGraphs)
-    print("Complete")
+        print("Converting into python friendly format")
+        weightedGraph = accel.mergeWeightGraphs(subGraphs)
+        print("Complete")
 
-    print("Weighted Graph built, writeing to file")
-    with open("weight.csv", "w") as weightFile:
-        for entry in weightedGraph:
-            weightFile.write(f'{str(entry[0])},{str(entry[1])},{str(entry[2])}\n')
-    print("Complete")
+        print("Weighted Graph built, writeing to file")
+        with open(weightedGraphFilename, "w") as weightFile:
+            for entry in weightedGraph:
+                weightFile.write(f'{str(entry[0])},{str(entry[1])},{str(entry[2])}\n')
+        print("Complete")
+    else:
+        with open(weightedGraphFilename, "r") as weightFile:
+            for edge in weightFile:
+                values = edge.split(',')
+                weightedGraph.append((int(values[0]),int(values[1]),float(values[2])))
 
     return weightedGraph
 
-def clusterWeightedGraph(weightedGraph, edges, nodelist, edgelist):
+def clusterWeightedGraph(weightedGraph, edges, nodelist, edgelist, weightThreashold = 0.2):
     '''Use weighted graph to cluster nodes and generate a new catagorization graph.
         Nodes with the weight between them close to 1 should be clustered together. 
         A single node may be part of multiple clusters. 
@@ -136,8 +139,24 @@ def clusterWeightedGraph(weightedGraph, edges, nodelist, edgelist):
         These edges are directed and will store a symptom and a chariteristic weight.
         For an edge AB, the weight is the number of nodes in A that have an edge of the symptom to B divided by the number of nodes in A.
     '''
-    #Perge edges in weightedGraph below threashold
-    #Cluster weightedGraph
+    print("Purgeing edges between weakly related nodes")
+    weightedGraph = [x for x in weightedGraph if x[2] > weightThreashold]
+
+    print("Building snap graph")
+    snapWeightedGraph = snap.TUNGraph_New(len(nodelist), len(weightedGraph))
+    for i in range(0, len(nodelist)):
+        snapWeightedGraph.AddNode(i)
+    for edge in weightedGraph:
+        snapWeightedGraph.AddEdge(edge[0], edge[1])
+    print("Clustering weighted graph")
+    components = snap.TCnComV()
+    print(f'Mod: {snap.CommunityCNM(snapWeightedGraph, components)}\nComponet sizes: ')
+    for comp in components:
+        if comp.Len() > 2:
+            print(f'{len(comp)},', end='')
+    print(f'\ntotal {len(components)}')
+    
+    
     #Make new graph, nodes corisponding to clusters in weightedGraph
     #Made edges in this new graph containing a symptom and weight
     ##Nodes will have a list of nodes from base graph that are contained within the cluster
@@ -147,5 +166,5 @@ def clusterWeightedGraph(weightedGraph, edges, nodelist, edgelist):
 
 def main():
     edges, nodeList, edgeList = loadBaseGraph()
-    weightedGraph = genWeightedGraph(edges, nodeList, 10)
-    catagorizationGraph = clusterWeightedGraph(weightedGraph, edges, nodeList, edgeList)
+    weightedGraph = genWeightedGraph(edges, nodeList, 8)
+    catagorizationGraph = clusterWeightedGraph(weightedGraph, edges, nodeList, edgeList, 0.45)
